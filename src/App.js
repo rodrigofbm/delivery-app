172094017223ce68073b7ff9f7ee8f84d62dec7c
Rodrigo Torres <rodrigo_fbm@hotmail.com>
2021-04-27 17:51:41 -0300

    add splash screen

--- src/App.js
+++ src/App.js
@@ -1,7 +1,12 @@
-import React from 'react';
+import React, {useEffect}from 'react';
+import SplashScreen from 'react-native-splash-screen'
+
 import Login from "./screens/Login"
 
 const App = () => {
+  useEffect(() => {
+    SplashScreen.hide();
+  }, [])
   return <Login/>;
 }
 
